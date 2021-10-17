f73666c596eebeed057e89e040ca3f03de9be059
mikhail <mizolotu@jyu.fi>
2021-10-17 16:40:09 +0300

    17.10.2021

--- extract_features.py
+++ extract_features.py
@@ -284,7 +284,6 @@ if __name__ == '__main__':
             dst = spl[3]
             dport = int(spl[4])
             size = float(spl[5])
-            print(timestamp, src, dst, sport, dport, size)
 
             if sport in ports and dst.startswith(subnet):
                 id = [dst, dport, src, sport]
@@ -322,8 +321,8 @@ if __name__ == '__main__':
 
                     # print features
 
-                    for i, f in zip(flow_ids, flow_features):
-                        print(i, f)
+                    #for i, f in zip(flow_ids, flow_features):
+                    #    print(i, f)
 
                     # update time
 
