ae3c38919d9e93778eb3de6d2647a433e97dce77
mikhail <mizolotu@jyu.fi>
2021-10-18 19:39:48 +0300

    18.10.2021

--- create_dataset.py
+++ create_dataset.py
@@ -5,6 +5,7 @@ from config import attacker_ip
 if __name__ == '__main__':
 
     for line in iter(sys.stdin.readline, b''):
+        print(line)
         try:
             spl = line.strip().split(',')
             features = ','.join(spl[4:])
