f4bef1888175604fb2ce2136817387a7a04d13aa
mikhail <mizolotu@jyu.fi>
2021-10-18 19:35:06 +0300

    18.10.2021

---
+++ config.py
@@ -0,0 +1,18 @@
+# sflow collector parameters
+
+collector_ip = "127.0.0.1"
+collector_port = 6343
+
+# feature extraction parameters
+
+subnet = '192.168.10.'  # for the advanced assignment change to '192.168.12.'
+ports = [80, 443]
+step = 3
+thr = 3
+
+# dataset creation parameters
+
+attacker_ip = '192.168.12.2'
+
+# ml parameters
+
