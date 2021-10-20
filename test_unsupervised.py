9055031255701ad251ea98155729d5e4cb2b8514
mikhail <mizolotu@jyu.fi>
2021-10-20 21:24:21 +0300

    20.10.2021

--- test_unsupervised.py
+++ test_unsupervised.py
@@ -26,8 +26,9 @@ def classify(q, model, step):
                 predictions = np.clip(probs / thr, 0, 1)
                 sys.stdout.write('Probability of a reverse shell:\n')
                 sys.stdout.flush()
-                for id, pr in zip(ids, predictions):
-                    line = f'{id} - {pr * 100} %\n'
+                idx = np.argsort(predictions)[::-1]
+                for i in idx:
+                    line = f'{ids[i]} - {predictions[i] * 100} %\n'
                     sys.stdout.write(line)
                     sys.stdout.flush()
             ids, batch = [], []
