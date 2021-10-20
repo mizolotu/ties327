7e2dd7351403991339196fd514a9189fb02facbd
mikhail <mizolotu@jyu.fi>
2021-10-20 21:13:41 +0300

    20.10.2021

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import time
-import datetime
-
-while True:
-    line = sys.stdin.readline()
-    sys.stdout.write(str(datetime.datetime.now().strftime("%H:%M:%S"))+'\t')
-    sys.stdout.write(line)
-    sys.stdout.flush()
